263ca1926e4651b9d79da0164c1adef82297cedf
s.miroshnikov <97443660+s-miroshnikov@users.noreply.github.com>
2022-12-21 19:38:53 +0400

    Update array.sh

--- array.sh
+++ array.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-# Place your code here
+f_count=$1
+# we will use array with alphabet
+read -ra l_array <<< "$(echo {a..z})"
+for ((l=0;l<=f_count-1;l++))
+do
+mkdir folder_"${l_array[$l]}"
+done
